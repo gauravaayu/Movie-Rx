0f89e1cbc264805c4a37106dc9d7abc08ace99aa
Gaurav <mishrabhonu0143@gmail.com>
2023-05-19 21:00:30 +0530

    error fixed

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import {getApiConfiguration, getGenres} from './store/homeSlice';
 import { useSelector, useDispatch } from 'react-redux';
 import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
-import PageNotfound from "./pages/404/PageNotFound";
+import PageNotFound from "./pages/404/PageNotFound";
 import Details from "./pages/details/Details";
 import Explore from "./pages/explore/Explore";
 import Home from "./pages/home/Home";
